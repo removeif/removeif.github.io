41e710995b1b9f27b4041cf2f640a2b5b3c48034
liyuechao <yuechao.li@mail.dealmoon.com>
2020-02-25 12:54:14 +0800

    Site updated: 2020-02-25 12:54:12

--- js/friend.js
+++ js/friend.js
@@ -1,7 +1,7 @@
 // author by removef
 // https://removeif.github.io/
 $(function () { //获取处理友链数据
-    ajaxReqForGitHub("https://api.github.com/repos/removeif/issue_database/issues/2/comments?per_page=100",function (data) {
+    ajaxReqForGitHub("https://api.github.com/repos/removeif/issue_database/issues/2/comments?per_page=100", function (data) {
         loadFriends(data);
     });
 });
@@ -20,7 +20,10 @@ function loadFriends(source) {
     // 随机排序过滤失效的
     let notValid = data.filter((item, a, b) => item.valid == 0);
 
-    let hasWeightData = data.filter((item, a, b) => item.weight != undefined && item.weight > 0).sort(function (a, b) {
+    let hasWeightData = data.filter((item, a, b) => item.valid != 0 && item.weight != undefined && item.weight > 0).sort(function (a, b) {
+        if (b.weight == a.weight) {
+            return a.date.localeCompare(b.date);
+        }
         return b.weight - a.weight;
     });
 
@@ -41,7 +44,7 @@ function loadFriends(source) {
             html += "    <img class=\"ava\" src=\"" + e.src + "\">";
         }
         html +=
-            "<div class='text-desc' title=\""+e.desc+"\">    网址：<a href=\"" + e.url + "\" target=\"_blank\">" + e.name + "</a>" +
+            "<div class='text-desc' title=\"" + e.desc + "\">    网址：<a href=\"" + e.url + "\" target=\"_blank\">" + e.name + "</a>" +
             "    <br>时间：" + e.date +
             "<br>简介：" + e.desc + "</div>" +
             "    </div>";
@@ -56,7 +59,7 @@ function loadFriends(source) {
             var html = "<div class=\"friend-card-item\">";
             html += "    <img class=\"ava\" src=\"/img/links/nopic.jpg\" title=\"图片链接不可用，使用的默认图片\">";
             html +=
-                "<div class='text-desc' title=\""+e.desc+"\">    网址：<a href=\"" + e.url + "\" target=\"_blank\">" + e.name + "</a>" +
+                "<div class='text-desc' title=\"" + e.desc + "\">    网址：<a href=\"" + e.url + "\" target=\"_blank\">" + e.name + "</a>" +
                 "    <br>访问时间：" + e.stopTime +
                 "<br>简介：" + e.desc + "</div>" +
                 "    </div>";
